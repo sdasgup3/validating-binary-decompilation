89aa1d119beb8873d534addfce012779b34ba791
Sandeep Dasgupta <sdasgup3@illinois.edu>
2019-05-22 22:01:40 -0500

    collect the revng logs

--- scripts/mcsema_out.sh
+++ scripts/mcsema_out.sh
@@ -7,3 +7,10 @@ IDA=/home/sdasgup3/ida-6.95/idal64
 mcsema-disass --disassembler $IDA --os linux --arch amd64 --output $bin.cfg --binary $bin --entrypoint $entry
 mcsema-lift-4.0 --os linux --arch amd64 --cfg $bin.cfg --output $bin.bc
 llvm-dis $bin.bc -o $bin.ll
+
+
+## revng
+/home/sdasgup3/Github/orchestra
+. ./environment
+/home/sdasgup3/Github/orchestra/root/usr/x86_64-pc-linux-gnu/x86_64-gentoo-linux-musl/gcc-bin/4.9.3/x86_64-gentoo-linux-musl-gcc -static getsign.c -o getsign.x86-64
+/home/sdasgup3/Github/orchestra/build/revng/revng translate -O0 getsign.x86-64
