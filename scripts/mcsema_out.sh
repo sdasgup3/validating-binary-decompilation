b788b773044f1056e515bf3573479ddbd7e308b3
Sandeep Dasgupta <sdasgup3@illinois.edu>
2019-07-31 08:30:44 -0500

    Issue:https://github.com/sdasgup3/validating-binary-decompilation/issues/2:Adding x86 dfg plots and makefiles to create various artifacts

--- scripts/mcsema_out.sh
+++ scripts/mcsema_out.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 set -x
 
-bin=$1
+# Examples:
+# To run mcsema on a binary `binlabel` with entry point and `entry`
+#   $ ./run-all-tests.sh
+# Note: the outputs will be generated in the current directory
+
+binary=$1
+binlabel=$(basename $binary)
 entry=$2
 IDA=/home/sdasgup3/ida-6.95/idal64
-mcsema-disass --disassembler /home/sdasgup3/ida-6.95/idal64 --os linux --arch amd64 --output $bin.cfg --binary $bin --entrypoint $entry
-#mcsema-lift-4.0 --os linux --arch amd64 --cfg $bin.cfg --output $bin.bc -disable_dead_store_elimination -disable_register_forwarding -disable_optimizer -keep_memops
-#mcsema-lift-4.0 --os linux --arch amd64 --cfg $bin.cfg --output $bin.bc -disable_register_forwarding -disable_dead_store_elimination # now mcsema have -enable_register_forwarding
-mcsema-lift-4.0 --os linux --arch amd64 --cfg $bin.cfg --output $bin.bc -disable_dead_store_elimination -disable_optimizer_O3
-llvm-dis $bin.bc -o $bin.unoptimized.ll
-mcsema-lift-4.0 --os linux --arch amd64 --cfg $bin.cfg --output $bin.bc -disable_optimizer_O3
-llvm-dis $bin.bc -o $bin.dead_stores_eliminated.ll
-mcsema-lift-4.0 --os linux --arch amd64 --cfg $bin.cfg --output $bin.bc
-llvm-dis $bin.bc -o $bin.optimized.ll
+mcsema-disass --disassembler /home/sdasgup3/ida-6.95/idal64 --os linux --arch amd64 --output $binlabel.cfg --binary $binary --entrypoint $entry
+#mcsema-lift-4.0 --os linux --arch amd64 --cfg $binlabel.cfg --output $binlabel.bc -disable_dead_store_elimination -disable_register_forwarding -disable_optimizer -keep_memops
+#mcsema-lift-4.0 --os linux --arch amd64 --cfg $binlabel.cfg --output $binlabel.bc -disable_register_forwarding -disable_dead_store_elimination # now mcsema have -enable_register_forwarding
+mcsema-lift-4.0 --os linux --arch amd64 --cfg $binlabel.cfg --output $binlabel.bc -disable_dead_store_elimination -disable_optimizer_O3
+llvm-dis $binlabel.bc -o $binlabel.unoptimized.ll
+mcsema-lift-4.0 --os linux --arch amd64 --cfg $binlabel.cfg --output $binlabel.bc -disable_optimizer_O3
+llvm-dis $binlabel.bc -o $binlabel.dead_stores_eliminated.ll
+mcsema-lift-4.0 --os linux --arch amd64 --cfg $binlabel.cfg --output $binlabel.bc
+llvm-dis $binlabel.bc -o $binlabel.optimized.ll
 
 #/home/sdasgup3/ida-6.95/idal64 -Lidal64.log -B -S"/usr/local/lib/python2.7/dist-packages/mcsema_disass-2.0-py2.7.egg/mcsema_disass/ida/get_cfg.py --output x.cfg --log_file x.log --arch amd64 --os linux --entrypoint main" get_sign
 
@@ -20,5 +26,5 @@ llvm-dis $bin.bc -o $bin.optimized.ll
 ## revng
 #/home/sdasgup3/Github/orchestra
 #. ./environment
-#/home/sdasgup3/Github/orchestra/root/usr/x86_64-pc-linux-gnu/x86_64-gentoo-linux-musl/gcc-bin/4.9.3/x86_64-gentoo-linux-musl-gcc -static getsign.c -o getsign.x86-64
+#/home/sdasgup3/Github/orchestra/root/usr/x86_64-pc-linux-gnu/x86_64-gentoo-linux-musl/gcc-binlabel/4.9.3/x86_64-gentoo-linux-musl-gcc -static getsign.c -o getsign.x86-64
 #/home/sdasgup3/Github/orchestra/build/revng/revng translate -O0 getsign.x86-64
--- tests/get_sign/get_sign.c
+++
@@ -1,9 +0,0 @@
-int get_sign(int x) {
-  if(x < 1) return 1;
-  else return 0;
-  
-}
-
-int main() {
-  return get_sign(10);
-}
