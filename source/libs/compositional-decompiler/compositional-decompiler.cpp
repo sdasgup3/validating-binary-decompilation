51c4a5ceb7aa6c7e3bfc32ad0eaa6bfeecd08c5c
Sandeep Dasgupta <sdasgup3@illinois.edu>
2019-10-02 13:44:38 -0500

    Issue:  https://github.com/sdasgup3/validating-binary-decompilation/issues/15: WIP II

--- source/libs/compositional-decompiler/compositional-decompiler.cpp
+++ source/libs/compositional-decompiler/compositional-decompiler.cpp
@@ -12,9 +12,11 @@
 // C++ includes
 #include <cassert>
 #include <cstdlib>
+#include <dirent.h>
 #include <fstream>
 #include <iostream>
 #include <memory>
+#include <regex>
 #include <string>
 #include <vector>
 
@@ -26,6 +28,7 @@
 #include "src/ext/cpputil/include/io/console.h"
 #include "src/ext/cpputil/include/system/terminal.h"
 #include "src/ext/x64asm/include/x64asm.h"
+#include "src/specgen/specgen.h"
 
 using namespace std;
 using namespace stoke;
@@ -36,20 +39,30 @@ static string tempfile(const string &temp);
 static void to_dot(Cfg *cfg, const string &dot_file);
 static bool to_pdf(const string &dot_file, const string &pdf_file);
 static bool view_pdf(const string &pdf_file);
+// static stringstream &operator<<(stringstream &ss, const redi::ipstream &ips);
+vector<string> &extractFromStream(vector<string> &ss, redi::ipstream &ips);
+static x64asm::R64 r_to_r64(const x64asm::R &reg);
 
-CompositionalDecompiler::CompositionalDecompiler(string inPath,
-                                                 string outLLVMPath,
-                                                 string extractedFunction,
-                                                 bool flat_binary) {
+CompositionalDecompiler::CompositionalDecompiler(
+    const string &inPath, const string &outLLVMPath,
+    const string &extractedFunction, const string &singleInstrDecompPath,
+    bool flat_binary)
+    : scriptsPath("/home/sdasgup3/Github/validating-binary-decompilation/"
+                  "single_instruction_translation_validation/scripts/") {
   this->flat_binary = flat_binary;
   this->extractedFunction = extractedFunction;
+  this->singleInstrDecompPath = singleInstrDecompPath;
   cfg = NULL;
   clear_error();
-  disassemble(inPath);
+  if (!disassemble(inPath))
+    return;
   decompile(outLLVMPath);
 }
 
 void CompositionalDecompiler::computePCUpdates() {
+
+  Console::msg() << "Computing Label PC values ...\n";
+
   x64asm::Code code_ = cfg->get_code();
   auto fxn = cfg->get_function();
   uint64_t rip_offset_ = fxn.get_rip_offset();
@@ -74,6 +87,8 @@ void CompositionalDecompiler::computePCUpdates() {
   }
   Console::msg() << "\n";
 #endif
+
+  Console::msg() << "Computing Label PC values: Done.\n\n";
 }
 
 static void createCFG(const FunctionCallbackData &data, void *arg) {
@@ -94,43 +109,128 @@ static void createCFG(const FunctionCallbackData &data, void *arg) {
   }
 }
 
-void CompositionalDecompiler::disassemble(string inPath) {
-  Disassembler d;
+bool CompositionalDecompiler::disassemble(string inPath) {
+  Console::msg() << "Disassembling " + inPath << "\n";
 
+  Disassembler d;
   struct CallbackValue val(&cfg, extractedFunction);
   d.set_function_callback(createCFG, &val);
   d.set_flat_binary(flat_binary);
   d.disassemble(inPath);
 
+  if (!cfg) {
+    Console::error(1) << "CFG not yet created!" << endl;
+    return false;
+  }
+
+  Console::msg() << "Disassembled output\n";
+  Console::msg() << cfg->get_function();
+
   if (d.has_error()) {
     Console::error(1) << "Error: " << d.get_error() << endl;
+    return false;
+  }
+
+  Console::msg() << "Disassembling Done.\n\n"
+                 << "\n";
+  return true;
+}
+
+void CompositionalDecompiler::modifyTemplateDefinition(
+    vector<string> &local_defn, int fromIdx, int toIdx) {
+
+  Console::msg() << "\tmodifyTemplateDefinition: From: " + to_string(fromIdx) +
+                        " To: " + to_string(toIdx) + "\n";
+
+  regex relevantInst(".*getelementptr.*");
+  regex r(" " + to_string(fromIdx) + "$");
+
+  for (auto &line : local_defn) {
+    if (regex_match(line, relevantInst)) {
+      auto mod_line = std::regex_replace(line, r, " " + to_string(toIdx));
+      if (mod_line != line) {
+        Console::msg() << "\t\tBefore...\n";
+        Console::msg() << "\t\t" << line << "\n";
+        Console::msg() << "\t\tAfter...\n";
+        Console::msg() << "\t\t" << mod_line << "\n";
+      }
+    }
   }
 }
 
-void CompositionalDecompiler::decompile(string outLLVMPath) {
-  if (!cfg) {
-    Console::error(1) << "CFG not yet created!" << endl;
-    return;
+// %call1 = call %struct.Memory* @routine_pushq_RBP(%struct.State* %0, i64
+// %LPC1, %struct.Memory* %2)
+void CompositionalDecompiler::decompileInstruction(
+    x64asm::Instruction instr, x64asm::Instruction template_instr) {
+
+  stringstream ss_local_body;
+  string local_body("");
+  vector<string> local_defn;
+
+  auto templateDefnPath = findFileForOpcode(instr, singleInstrDecompPath);
+  auto cmd = scriptsPath + "/specialize_template.pl   --file " +
+             templateDefnPath + "/test.mod.ll";
+  redi::ipstream *stream = NULL;
+  run_command(cmd, true, &stream);
+  extractFromStream(local_defn, *stream);
+
+  Console::msg() << "\tExtracted Defintion: \n";
+  for (auto line : local_defn) {
+    Console::msg() << line << "\n";
   }
 
-  // Compute PC updates for each instruction
-  computePCUpdates();
+  delete stream;
 
-  // Generating the global struct definitions and intricsics declarations
-  string cmd = "cp "
-               "${HOME}/Github/validating-binary-decompilation/source/libs/"
-               "compositional-decompiler/data/mcsema_template.txt " +
-               outLLVMPath;
-  if (!run_command(cmd)) {
-    Console::error(1) << "Error: " << get_error() << endl;
-    return;
+  ss_local_body << "%call1 = call %struct.Memory* @routine_"
+                << instr.get_opcode() << "_";
+
+  string newFuncName("");
+  string extraArgs("");
+
+  for (size_t i = 0; i < instr.arity(); i++) {
+    auto instr_t = instr.type(i);
+    // auto template_instr_t = template_instr.type(i);
+
+    if (is_gpr_type(instr_t)) {
+      const R &R_OPR = instr.get_operand<R>(i);
+      int index_from = getMcSemaIndices(template_instr.get_operand<R>(i));
+      int index_to = getMcSemaIndices(R_OPR);
+
+      // Modify the defintion
+      modifyTemplateDefinition(local_defn, index_from, index_to);
+
+      // Modify the call
+      stringstream ss_reg_opr;
+      ss_reg_opr << R_OPR;
+      newFuncName += ss_reg_opr.str() + "_";
+    }
   }
+
+  // Dump the Body and the Defn
+  Body << ss_local_body.str() << newFuncName
+       << "(%struct.State* %0,i64  0, %struct.Memory* %2";
+
+  if (extraArgs != "") {
+    Body << extraArgs;
+  }
+  Body << ");\n\n";
+
+  for (auto line : local_defn) {
+    Defns << line << "\n";
+  }
+}
+
+void CompositionalDecompiler::decompileFunction(
+    const string &extractedFunction) {
+
+  Console::msg() << "Decompiling Function: " + extractedFunction + "...\n";
+
+  Body << "define %struct.Memory* @" + extractedFunction +
+              "(%struct.State* noalias , i64, "
+              "%struct.Memory* noalias) alwaysinline  {\n";
 
   // Iterate over instructions and generate calls to specialized instruction
   // semantics.
-  stringstream body;
-  stringstream defns;
-
   x64asm::Code code_ = cfg->get_code();
   auto fxn = cfg->get_function();
   uint64_t rip_offset_ = fxn.get_rip_offset();
@@ -147,32 +247,97 @@ void CompositionalDecompiler::decompile(string outLLVMPath) {
     assert(rip_offset_it != rip_offset_ite && size_it != size_ite &&
            "CompositionalDecompiler::decompile: Problem with iterators");
 
-    stringstream ss_code;
-    stringstream ss_rip;
-    stringstream ss_size;
+    auto instr = code_[code_it];
+    auto opcode = instr.get_opcode();
 
-    ss_code << code_[code_it];
+    Console::msg() << "Decompiling instr:" << instr << "...\n";
+
+    // Generate comment
+    stringstream ss_code, ss_rip, ss_size, ss_opcode;
+
+    ss_code << instr;
     ss_rip << rip_offset_ + *rip_offset_it;
     ss_size << *size_it;
+    ss_opcode << opcode;
 
-    body << "\t# Code: " << ss_code.str();
-    body << "\t# RIP: " << hex << showbase << ss_rip.str();
-    body << "\t# Bytes: " << dec << ss_size.str();
-    body << "\n";
+    Body << "\t# Code: " << ss_code.str();
+    Body << "\t# RIP: " << hex << showbase << ss_rip.str();
+    Body << "\t# Bytes: " << dec << ss_size.str();
+    Body << "\n";
+
+    if (opcode == LABEL_DEFN) {
+      Body << "\tblock_" << hex << ss_rip.str() << ":\n";
+      continue;
+    }
+
+    // Generate call to specialized instrcution semantics
+    auto template_instr = get_instruction_from_string(ss_opcode.str(), false);
+    Console::msg() << "\tw.r.t template instr:" << template_instr << "...\n";
+
+    if (!sanity_check_template_instruction(instr, template_instr)) {
+      Console::error(1) << "Mismatch :" << endl;
+      Console::error(1) << "Orginal: " << instr;
+      Console::error(1) << "Template: " << template_instr;
+      Console::error(1) << endl;
+      set_error("Mismatch between template and original instruction!\n");
+      continue;
+    }
+
+    decompileInstruction(instr, template_instr);
+  }
+
+  Console::msg() << "Decompiling Function: " + extractedFunction + "Done.\n\n";
+}
+
+bool CompositionalDecompiler::sanity_check_template_instruction(
+    x64asm::Instruction i1, x64asm::Instruction i2) {
+  if ((i1.get_opcode() != i2.get_opcode()) || (i1.arity() != i2.arity()))
+    return false;
+
+  for (size_t i = 0; i < i1.arity(); i++) {
+    auto instr_t = i1.type(i);
+    auto template_instr_t = i2.type(i);
+
+    if (instr_t != template_instr_t) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+void CompositionalDecompiler::decompile(string outLLVMPath) {
+
+  Console::msg() << "Decompiling " + outLLVMPath + "\n";
+
+  // Compute PC updates for each instruction
+  computePCUpdates();
+
+  // Generating the global struct definitions and intricsics declarations
+  string cmd = "cp "
+               "${HOME}/Github/validating-binary-decompilation/source/libs/"
+               "compositional-decompiler/data/mcsema_template.txt " +
+               outLLVMPath;
+  if (!run_command(cmd)) {
+    Console::error(1) << "Error: " << get_error() << endl;
+    return;
+  }
+
+  decompileFunction(extractedFunction);
+  if (has_error()) {
+    Console::error(1) << error_message_;
+    return;
   }
 
   // Generatng decompilation io text
   std::ofstream fd;
   fd.open(outLLVMPath.c_str(), ios::out | ios::app);
-  string fileContent = "define %struct.Memory* @" + extractedFunction +
-                       "(%struct.State* noalias , i64, "
-                       "%struct.Memory* noalias) alwaysinline  {\n";
-  fileContent += body.str();
-  fileContent += "}\n";
-  fileContent += defns.str();
-
-  fd << fileContent;
+  fd << Body.str();
+  fd << "}\n";
+  fd << Defns.str();
   fd.close();
+
+  Console::msg() << "Decompiling: Done.\n\n";
 }
 
 void CompositionalDecompiler::displayCFG(bool view) {
@@ -194,7 +359,8 @@ void CompositionalDecompiler::displayCFG(bool view) {
 }
 
 // redi::ipstream *CompositionalDecompiler::run_command(const string &cmd) {
-bool CompositionalDecompiler::run_command(const string &cmd) {
+bool CompositionalDecompiler::run_command(const string &cmd, bool ret_stream,
+                                          redi::ipstream **retval) {
   Console::msg() << "Executing: " << cmd << "\n";
   auto stream = new redi::ipstream(cmd, redi::pstreams::pstdout);
   if (!stream) {
@@ -209,11 +375,105 @@ bool CompositionalDecompiler::run_command(const string &cmd) {
     return false;
   }
 
-  delete stream;
+  if (!ret_stream) {
+    delete stream;
+  } else {
+    assert(retval && "NULL VALUE PASSED");
+    *retval = stream;
+  }
   // return stream;
   return true;
 }
 
+int CompositionalDecompiler::getMcSemaIndices(const R &reg) {
+  R64 r = r_to_r64(reg);
+  switch (r) {
+  case Constants::rax():
+    return 1;
+  case Constants::rbx():
+    return 3;
+  case Constants::rcx():
+    return 5;
+  case Constants::rdx():
+    return 7;
+  case Constants::rsi():
+    return 9;
+  case Constants::rdi():
+    return 11;
+  case Constants::rsp():
+    return 13;
+  case Constants::rbp():
+    return 15;
+  case Constants::r8():
+    return 17;
+  case Constants::r9():
+    return 19;
+  case Constants::r10():
+    return 21;
+  case Constants::r11():
+    return 23;
+  case Constants::r12():
+    return 25;
+  case Constants::r13():
+    return 27;
+  case Constants::r14():
+    return 29;
+  case Constants::r15():
+    return 31;
+  // case Constants::rip():
+  //  return 33;
+  default:
+    Console::error(1) << reg << "\n";
+    assert(0 && "Unknown register\n");
+  }
+  return -1;
+}
+
+int CompositionalDecompiler::getMcSemaIndices(const Sse &reg) { return -1; }
+
+string CompositionalDecompiler::findFileForOpcode(
+    x64asm::Instruction instr, const string &singleInstrDecompPath) {
+
+  string potDir(singleInstrDecompPath);
+  stringstream ss_opcode;
+  ss_opcode << instr.get_opcode();
+
+  Console::msg() << "Finding template file for " << ss_opcode.str() << "...\n";
+
+  if (is_any_operand_mem_type(instr)) {
+    potDir += "/memory-variants/";
+  } else if (is_any_operand_imm_type(instr)) {
+    potDir += "/immediate-variants/";
+  } else if (is_any_operand_gpr_type(instr)) {
+    potDir += "/register-variants/";
+  } else {
+    potDir += "/system-variants/";
+  }
+
+  Console::msg() << "\tFound Dir: " << potDir << "\n";
+
+  DIR *dir;
+  struct dirent *ent;
+  if ((dir = opendir(potDir.c_str())) == NULL) {
+    Console::error(1) << "Cannot open " << potDir << "\n";
+    exit(1);
+    return "";
+  }
+
+  /* print all the files and directories within directory */
+  while ((ent = readdir(dir)) != NULL) {
+    if (ent->d_name == ss_opcode.str()) {
+      Console::msg() << "\tFound Path: " << potDir + ss_opcode.str() << "\n\n";
+      return potDir + ss_opcode.str();
+    }
+  }
+  closedir(dir);
+
+  Console::error(1) << "Cannot find template file for " << ss_opcode.str()
+                    << "\n";
+  exit(1);
+  return "";
+}
 /*********************************************************************
 ************************* General Utilities **************************
 *********************************************************************/
@@ -221,7 +481,7 @@ string tempfile(const string &temp) {
   vector<char> v(temp.begin(), temp.end());
   v.push_back('\0');
 
-  const auto fd = mkstemp(v.data());
+  // const auto fd = mkstemp(v.data());
   return string(v.begin(), v.end() - 1);
 }
 
@@ -244,6 +504,31 @@ bool view_pdf(const string &pdf_file) {
   return term.result() == 0;
 }
 
+x64asm::R64 r_to_r64(const x64asm::R &reg) {
+  size_t idx = reg;
+  if (reg.type() == Type::RH) {
+    return Constants::r64s()[idx - 4];
+  }
+  return Constants::r64s()[idx];
+}
+
+// stringstream &operator<<(stringstream &ss, redi::ipstream &ips) {
+//  string line;
+//  while (getline(ips, line)) {
+//    ss << line;
+//  }
+//  return ss;
+//}
+
+vector<string> &extractFromStream(vector<string> &ss, redi::ipstream &ips) {
+  string line;
+  while (getline(ips, line)) {
+    // ss << line;
+    // ss << "\n";
+    ss.push_back(line);
+  }
+  return ss;
+}
 /************ Not used code *************
 
   // Add nodes corresponding to instrcutons
