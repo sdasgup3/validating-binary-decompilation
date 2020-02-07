355e73d15a8737a4a1957071d36ca6eb2330b5cc
Sandeep Dasgupta <sdasgup3@users.noreply.github.com>
2020-02-07 14:03:00 -0600

    Update README.md

--- tests/program_translation_validation/single-source-benchmark/docs/reuse.r
+++ tests/program_translation_validation/single-source-benchmark/docs/reuse.r
@@ -2,13 +2,13 @@ library(ggplot2)
 library(grid)
 library(gridExtra)
 
-pdf(file = "reuse.pdf")
+pdf(file = "docs/reuse.png")
 df <- read.table("docs/reuse_percentage.log", header = FALSE)
 
 data <- as.vector(df[,1])
 #plot(data)
 
 counts <- table(data)
-barplot(counts, main="Distribution of Reuse Percentage within & across functions\nfor a given sequence of execution of compositional decompiler\non test-subjects (i.e. functions)", xlab="Perc. of instructions reused", ylab="# of test-subjects(functions)")
+barplot(counts, xlab="Perc. of instructions reused", ylab="# of test-subjects(functions)")
 
 dev.off()
