622b79dbc35a2602127b3b2013545b86cbe92179
Sandeep Dasgupta <sdasgup3@illinois.edu>
2020-02-28 20:20:51 -0600

    AE PLDI20: commiting local changes for sym-ex memory instr: Finalized DONE

--- tests/scripts/matcher_driver.sh
+++ tests/scripts/matcher_driver.sh
@@ -18,10 +18,10 @@ selectPassSeq() {
              -simplifycfg -basicaa -aa -instcombine"
 
   ## More effective pass list derived after submission.
-  NORM_PASS="-mem2reg -licm -gvn -early-cse -globalopt -simplifycfg -basicaa  \
-             -aa -memdep -dse -deadargelim -libcalls-shrinkwrap -tailcallelim \
-             -simplifycfg -basicaa -aa -instcombine -simplifycfg -early-cse   \
-             -gvn -basicaa -aa -memdep -dse -memcpyopt"
+#  NORM_PASS="-mem2reg -licm -gvn -early-cse -globalopt -simplifycfg -basicaa  \
+#             -aa -memdep -dse -deadargelim -libcalls-shrinkwrap -tailcallelim \
+#             -simplifycfg -basicaa -aa -instcombine -simplifycfg -early-cse   \
+#             -gvn -basicaa -aa -memdep -dse -memcpyopt"
 
   if [ -v NORM ]; then
     if [ "$NORM" != "CUSTOM" ]; then
